37dc8388c70f9e267b4956d352ee044f2dfd0e49
Shadi zgheib <54122855+shadizghe@users.noreply.github.com>
2024-08-07 10:46:50 -0700

    Delete Shoes/TheTechHeads directory

--- Shoes/TheTechHeads/public/payment.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('payment-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Payment successful!');
-    // Here you can add code to process the payment
-});
