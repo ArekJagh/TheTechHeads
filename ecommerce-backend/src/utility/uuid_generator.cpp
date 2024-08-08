7a0eedee7c4e87f246160bcf2bba2ee0631cc1bf
John <32312422+aaarr21@users.noreply.github.com>
2024-08-07 17:01:42 -0700

    Delete ecommerce-backend directory
    need to do modifications

--- ecommerce-backend/src/utility/uuid_generator.cpp
+++
@@ -1,8 +0,0 @@
-#include "uuid_generator.h"
-
-#include <sstream>
-
-std::string UUIDGenerator::generateUUID(){
-    boost::uuids::uuid uuid = boost::uuids::random_generator()();
-    return to_string(uuid);
-}
