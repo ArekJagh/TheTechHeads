8ab1de603cd192f81919ea8ecf675294b0df5033
John <32312422+aaarr21@users.noreply.github.com>
2024-08-20 13:50:19 -0700

    Delete Java GUI/JAVA directory

--- Java GUI/JAVA/ProductDAO.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-
-public interface ProductDAO extends GenericDAO<Product, String> {
-    Product findByName (String name);
-    List<Product> findByCategory (String category);
-
-}
--- Java GUI/JAVA/UserDAO.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-
-public interface UserDAO extends GenericDAO<User, String> {
-    User findByUsername(String username);
-    User findByEmail(String email);
-    List<User> findByLastName(String lastName);
-}
